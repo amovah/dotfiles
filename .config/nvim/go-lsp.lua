a31edf72b355534664aba27689eab8922680a31d
Ali Movahedi <movahedi.4li@gmail.com>
2022-03-07 01:47:18 +0330

    add gopls configs

--- .config/nvim/go-lsp.lua
+++ .config/nvim/go-lsp.lua
@@ -1,6 +1,8 @@
 lspconfig = require "lspconfig"
 lspconfig.gopls.setup {
     cmd = {"gopls", "serve"},
+    filetypes = {"go", "gomod"},
+    root_dir = util.root_pattern("go.work", "go.mod", ".git")
     settings = {
         gopls = {
             analyses = {
