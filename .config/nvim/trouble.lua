51fb918ce5cd04a2ff6823cc9fef116434c8e9ce
Ali Movahedi <ali_movahedi@aol.com>
2022-03-07 10:16:36 +0330

    change trouble hotkey to keybindings

--- .config/nvim/trouble.lua
+++ .config/nvim/trouble.lua
@@ -10,4 +10,4 @@ vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<c
 vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
 vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
-vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", opts)
+vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>", opts)
